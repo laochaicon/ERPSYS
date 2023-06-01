9bde4328315eec6682e5ece21c7e8fc7f1ae2ccd
老茶 <2086221580@qq.com>
2023-06-01 11:35:15 +0800

    实现类SysUserController登录功能； 增加了密码加密算法

--- src/main/java/com/hqyj/erp_sys/controller/SysUserController.java
+++ src/main/java/com/hqyj/erp_sys/controller/SysUserController.java
@@ -1,7 +1,14 @@
 package com.hqyj.erp_sys.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hqyj.erp_sys.entity.SysUser;
+import com.hqyj.erp_sys.service.ISysUserService;
+import com.hqyj.erp_sys.util.ResultData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -11,8 +18,34 @@ import org.springframework.stereotype.Controller;
  * @author 老茶
  * @since 2023-05-25
  */
-@Controller
-@RequestMapping("/sysUser")
+@RestController
+@CrossOrigin
 public class SysUserController {
+    @Autowired
+    private ISysUserService sysUserService;
+
+    @PostMapping("/login")
+    public ResultData login(SysUser sysUser){
+
+        String newPwd= DigestUtils.md5DigestAsHex(sysUser.getSuPassword().getBytes());
+        sysUser.setSuPassword(newPwd);
+
+        QueryWrapper<SysUser> wrapper = new QueryWrapper<>(sysUser);
+
+        //System.out.println(wrapper);
+        SysUser one = sysUserService.getOne(wrapper);
+
+        if(one!=null){
+            System.out.println(one);
+            return ResultData.ok("登录成功");
+        }
+        return ResultData.error(1,"用户名或密码错误");
+
+    }
+
+    /*public static void main(String[] args) {
+        String s=DigestUtils.md5DigestAsHex("admin".getBytes());
+        System.out.println(s);
+    }*/
 
 }
